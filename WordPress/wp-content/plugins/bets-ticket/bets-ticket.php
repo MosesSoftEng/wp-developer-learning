89ece28efee4997a54788bba01c5535589d54da2
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2022-08-12 10:09:34 +0300

    create plugin with header

---
+++ WordPress/wp-content/plugins/bets-ticket/bets-ticket.php
@@ -0,0 +1,11 @@
+<?php
+// Plugin header
+/**
+ * Plugin Name: Bets Tickets
+ * Description: A custom bets post plugin
+ * Version: 1.0.0
+ * Author: MosesSoftEng
+ * Author URI: https://twitter.com/MosesSoftEng
+ */
+
+ 
