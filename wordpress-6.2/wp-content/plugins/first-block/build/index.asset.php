8eb4b78e51410bb740521a6a5550ba89b84f4a72
Moses Mwangi <ngigemoses1@outlook.com>
2023-05-02 13:16:58 +0300

    Add multiple block styles

--- wordpress-6.2/wp-content/plugins/first-block/build/index.asset.php
+++ wordpress-6.2/wp-content/plugins/first-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0f90f2c59c1caad26507');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'de87fabd9c478ae0adf7');
